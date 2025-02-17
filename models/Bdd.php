490cf25bd59d748f29bb466deb1e74935dd5aaeb
Bossola-Yannick <bossolay@hotmail.fr>
2025-02-17 13:50:06 +0100

    feat: add class Bdd, header, footer, images and css header/footer

--- models/Bdd.php
+++ models/Bdd.php
@@ -1,3 +1,20 @@
 <?php
 
-class Bdd {}
+class Bdd
+{
+    protected string $host = "localhost";
+    protected string $username = "root";
+    protected string $password = "";
+    protected string $dbName = "livreor";
+
+    public function connexion()
+    {
+        try {
+            $bdd  = new PDO("mysql:host=$this->host;dbname=$this->dbName;charset=utf8", $this->username, $this->password);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $bdd;
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+        }
+    }
+}
